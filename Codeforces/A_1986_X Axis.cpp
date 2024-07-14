779c3b610b661f221920125a769019d74e33328e
Fahim Shahriar <fahim.cse273@gmail.com>
2024-07-15 00:14:36 +0600

    Add files via upload

---
+++ Codeforces/A_1986_X Axis.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+int t;
+cin>>t;
+while(t--){
+int a,b,c;
+cin>>a>>b>>c;
+int ar[3];
+
+ar[0]=abs(a-b);
+ar[1]=abs(c-b);
+ar[2]=abs(a-c);
+
+cout<<ar[0]+ar[1]+ar[2]<<endl;
+
+
+
+
+
+}
+
+
+
+
+
+}
